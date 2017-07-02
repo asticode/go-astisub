cfbefdf7c0c4dc92ba472b57960dfaaa3693d9f2
Quentin Renard <contact@asticode.com>
2017-07-02 15:51:02 +0200

    Added .srt

---
+++ srt.go
@@ -0,0 +1,135 @@
+package astisub
+
+import (
+	"bufio"
+	"io"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+// Constants
+const (
+	srtTimeBoundariesSeparator = " --> "
+)
+
+// Vars
+var (
+	bytesSRTTimeBoundariesSeparator = []byte(srtTimeBoundariesSeparator)
+)
+
+// parseDurationSRT parses an .srt duration
+func parseDurationSRT(i string) (time.Duration, error) {
+	return parseDuration(i, ",")
+}
+
+// ReadFromSRT parses an .srt content
+func ReadFromSRT(i io.Reader) (o *Subtitles, err error) {
+	// Init
+	o = &Subtitles{}
+	var scanner = bufio.NewScanner(i)
+
+	// Scan
+	var line string
+	var s = &Item{}
+	for scanner.Scan() {
+		// Fetch line
+		line = scanner.Text()
+
+		// Line contains time boundaries
+		if strings.Contains(line, srtTimeBoundariesSeparator) {
+			// Remove last item of previous subtitle since it's the index
+			s.Lines = s.Lines[:len(s.Lines)-1]
+
+			// Remove trailing empty lines
+			if len(s.Lines) > 0 {
+				for i := len(s.Lines) - 1; i >= 0; i-- {
+					if len(s.Lines[i]) > 0 {
+						for j := len(s.Lines[i]) - 1; j >= 0; j-- {
+							if len(s.Lines[i][j].Text) == 0 {
+								s.Lines[i] = s.Lines[i][:j]
+							} else {
+								break
+							}
+						}
+						if len(s.Lines[i]) == 0 {
+							s.Lines = s.Lines[:i]
+						}
+
+					}
+				}
+			}
+
+			// Init subtitle
+			s = &Item{}
+
+			// Fetch time boundaries
+			boundaries := strings.Split(line, srtTimeBoundariesSeparator)
+			if s.StartAt, err = parseDurationSRT(boundaries[0]); err != nil {
+				err = errors.Wrapf(err, "parsing srt duration %s failed", boundaries[0])
+				return
+			}
+			if s.EndAt, err = parseDurationSRT(boundaries[1]); err != nil {
+				err = errors.Wrapf(err, "parsing srt duration %s failed", boundaries[1])
+				return
+			}
+
+			// Append subtitle
+			o.Items = append(o.Items, s)
+		} else {
+			// Add text
+			s.Lines = append(s.Lines, []LineItem{{Text: line}})
+		}
+	}
+	return
+}
+
+// formatDurationSRT formats an .srt duration
+func formatDurationSRT(i time.Duration) string {
+	return formatDuration(i, ",")
+}
+
+// WriteToSRT writes subtitles in .srt format
+func (s Subtitles) WriteToSRT(o io.Writer) (err error) {
+	// Do not write anything if no subtitles
+	if len(s.Items) == 0 {
+		err = ErrNoSubtitlesToWrite
+		return
+	}
+
+	// Add BOM header
+	var c []byte
+	c = append(c, BytesBOM...)
+
+	// Loop through subtitles
+	for k, v := range s.Items {
+		// Add time boundaries
+		c = append(c, []byte(strconv.Itoa(k+1))...)
+		c = append(c, bytesLineSeparator...)
+		c = append(c, []byte(formatDurationSRT(v.StartAt))...)
+		c = append(c, bytesSRTTimeBoundariesSeparator...)
+		c = append(c, []byte(formatDurationSRT(v.EndAt))...)
+		c = append(c, bytesLineSeparator...)
+
+		// Loop through lines
+		for _, l := range v.Lines {
+			c = append(c, []byte(l.String())...)
+			c = append(c, bytesLineSeparator...)
+		}
+
+		// Add new line
+		c = append(c, bytesLineSeparator...)
+	}
+
+	// Remove last new line
+	c = c[:len(c)-1]
+
+	// Write
+	if _, err = o.Write(c); err != nil {
+		err = errors.Wrap(err, "writing failed")
+		return
+	}
+	return
+}
---
+++ srt_test.go
@@ -0,0 +1,29 @@
+package astisub_test
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+
+	"github.com/asticode/go-astisub"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSRT(t *testing.T) {
+	// Open
+	s, err := astisub.Open("./testdata/example-in.srt")
+	assert.NoError(t, err)
+	assertSubtitleItems(t, s)
+
+	// No subtitles to write
+	w := &bytes.Buffer{}
+	err = astisub.Subtitles{}.WriteToSRT(w)
+	assert.EqualError(t, err, astisub.ErrNoSubtitlesToWrite.Error())
+
+	// Write
+	c, err := ioutil.ReadFile("./testdata/example-out.srt")
+	assert.NoError(t, err)
+	err = s.WriteToSRT(w)
+	assert.NoError(t, err)
+	assert.Equal(t, string(c), w.String())
+}
